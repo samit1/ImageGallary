bdd68df34cc3271ea933b83747b0f9bc6c716304
Sami Taha <taha.sami.j@gmail.com>
2018-05-31 18:43:27 -0500

    Initial Commit

---
+++ ImageGallaryTests/ImageGallaryTests.swift
@@ -0,0 +1,36 @@
+//
+//  ImageGallaryTests.swift
+//  ImageGallaryTests
+//
+//  Created by Sami Taha on 5/31/18.
+//  Copyright © 2018 Sami Taha. All rights reserved.
+//
+
+import XCTest
+@testable import ImageGallary
+
+class ImageGallaryTests: XCTestCase {
+    
+    override func setUp() {
+        super.setUp()
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+    
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+        super.tearDown()
+    }
+    
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+    
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+    
+}
